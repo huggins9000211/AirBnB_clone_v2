f5b7b83bcec25df36c210d9f1fec1fdd5840274d
Anthony <huggins9000211@gmail.com>
2020-03-27 20:33:42 -0400

    first commit

---
+++ models/engine/file_storage.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""This is the file storage class for AirBnB"""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class FileStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self, obj=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        results = {}
+        if obj == None:
+            return self.__objects
+        print("*\n*\n*\n*\n*\n*\n{}*\n*\n*\n*\n*\n*\n*\n".format(obj.__name__))
+        for x, y in self.__objects.items():
+            if obj.__name__ == x.split(".")[0]:
+                results[x] = y
+        return results
+
+    def delete(self, obj=None):
+        self.__objects = {key:val for key, val in self.__objects.items() if val is not obj}
+
+    def new(self, obj):
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
+
+    def reload(self):
+        """serialize the file path to JSON file path
+        """
+        try:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
+            pass
