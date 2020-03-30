f8dfb7adb777f208b517aefb961eac61fa4147df
Anthony <huggins9000211@gmail.com>
2020-03-30 18:29:29 -0400

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,11 +1,59 @@
 #!/usr/bin/python3
 """This is the file storage class for AirBnB"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+import models.base_model
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.user import User
 
 
 class DBStorage():
     """ db """
     __engine = None
     __session = None
+    allTypes = {"User": User, "State": State, "City": City, "Amenity":\
+         Amenity, "Place": Place, "Review": Review}
 
     def __init__(self):
         """ init """
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(os.getenv(
+                'HBNB_MYSQL_USER'), os.getenv('HBNB_MYSQL_PWD'), os.getenv(
+                    'HBNB_MYSQL_HOST'), os.getenv(
+                        'HBNB_MYSQL_DB')), pool_pre_ping=True)
+        if os.getenv('HBNB_ENV') == 'test':
+            models.base_model.Base.metadata.drop_all(self.__engine)
+    
+    def all(self, cls=None):
+        """ all """
+        results = {}
+        if cls is None:
+            for name, x in self.allTypes.items():
+                for y in self.__session.query(x):
+                    results["{}.{}".format(name, y.id)] 
+            return results
+        for y in self.__session.query(self.allTypes[cls]):
+            results["{}.{}".format(cls, y.id)] 
+        return results
+    
+    def new(self, obj):
+        """ new """
+        self.__session.add(obj)
+    
+    def save(self):
+        """ save """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ reload """
+                
--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+import models
 
 
 class State(BaseModel, Base):
@@ -12,3 +13,11 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    def cities(self):
+        result = {}
+        allStates = models.storage.all(State)
+        for x, y in allStates.items():
+            if y.state_id == self.id:
+                result[x] = y
+        return result
