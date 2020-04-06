e2c69a02f5a812c03c795534f24b6c9ef2f1d42d
Anthony <huggins9000211@gmail.com>
2020-04-06 19:29:46 -0400

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #test
 place="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/hbnb_static/\n\t}"
+sudo apt-get update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo 'Hello World' > /data/web_static/releases/test/index.html
