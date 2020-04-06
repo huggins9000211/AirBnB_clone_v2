fcbfc76f8f015a7316cbf9a447e63366d791bee6
Anthony <huggins9000211@gmail.com>
2020-04-06 19:26:09 -0400

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 place="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/hbnb_static/\n\t}"
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo 'Hello World' > /data/web_static/releases/test/index.html
+echo 'Hello World' > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "37i\ $redirect" /etc/nginx/sites-available/default
+sudo sed -i "37i\ $place" /etc/nginx/sites-available/default
