6469ee2f9863708e73d90cea3a240e815245c6b4
Anthony <huggins9000211@gmail.com>
2020-04-06 20:15:44 -0400

    test

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" test """
+from fabric.api import local, settings
+import fabric
+import datetime
+
+
+def do_pack():
+    """ test """
+    try:
+        dt = datetime.datetime.now()
+        cname = "versions/web_static_{}{}{}{}{}.tgz".format(
+            dt.year, dt.month, dt.day, dt.hour, dt.minute
+        )
+        command = "tar -cvzf {} web_static".format(cname)
+        with settings(warn_only=True):
+            local("mkdir -p versions")
+            result = local(command)
+        if result.failed:
+            print("ahrwwaerha")
+            return None
+        return cname
+    except Exception:
+        return None
