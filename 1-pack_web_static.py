1e472b28b565aa321aa213c99681de9c8a5ee63b
Anthony <huggins9000211@gmail.com>
2020-04-06 20:18:22 -0400

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,17 +7,14 @@ import datetime
 
 def do_pack():
     """ test """
-    try:
-        dt = datetime.datetime.now()
-        cname = "versions/web_static_{}{}{}{}{}.tgz".format(
-            dt.year, dt.month, dt.day, dt.hour, dt.minute
-        )
-        command = "tar -cvzf {} web_static".format(cname)
-        with settings(warn_only=True):
-            local("mkdir -p versions")
-            result = local(command)
-        if result.failed:
-            return None
-        #return cname
-    except Exception:
+    dt = datetime.datetime.now()
+    cname = "versions/web_static_{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute
+    )
+    command = "tar -cvzf {} web_static".format(cname)
+    with settings(warn_only=True):
+        local("mkdir -p versions")
+        result = local(command)
+    if result.failed:
         return None
+    return cname
