cde7485a23874caa90d0dfc492edb2021b812bb8
Anthony <huggins9000211@gmail.com>
2020-04-21 18:29:15 -0400

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
+""" test """
 from flask import Flask, escape, request
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ test """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
